d58adb3227512bde385105b5120de10f692a064f
Ciaran Edwards <ciaran.edwards@live.com>
2017-02-14 16:55:20 +0000

    Task 2 functionality done

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article_count = Article.all.count
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(articles_params)
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(articles_params)
+    redirect_to article_path(params[:id])
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'articles/list'
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/delete'
+  # get 'articles/index'
+
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
